d07cf28619bbacf999b6b1edd9f58c7c52ad9f90
czh <supercaizehua@foxmail.com>
2022-08-29 00:27:18 +0800

    thread作业4

--- codes/threads/thread作业4.py
+++ codes/threads/thread作业4.py
@@ -1,5 +1,5 @@
 import time, threading
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED, as_completed
 
 
 def longtime(s):
@@ -12,10 +12,18 @@ def task_(s):
     t = threading.Thread(target=longtime, args=(s,))
     print('单个子线程开始')
     t.start()
+    return True
 
 
 with ThreadPoolExecutor(max_workers=3) as executor:
+    all_task = []
     for i in [1, 3, 5, 7]:
         task = executor.submit(task_, i)
+        all_task.append(task)
+
+    wait(all_task, return_when=ALL_COMPLETED)
+    for future in as_completed(all_task):
+        res = future.result()
+        print(res)
 
     print('finish')
